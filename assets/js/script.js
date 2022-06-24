01c23ac77eb4fbe9b13aa8c5b765ab1e7989b464
Claire Cashmore <clairecashmore2017@gmail.com>
2022-06-24 15:15:16 -0400

    successfully pulling from web api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log("This is the javascript for our weather page!");
+// Function to get the current weather stats
+var apiKey = "5594d308b30b4677a98190029222406";
+var weatherUrl = "http://api.weatherapi.com/v1/current.json?key=" + apiKey + "&q=New York&days=5&";
+
+fetch(weatherUrl)
+.then(function(response){
+    if(response.ok){
+        response.json()
+        .then(function(data){
+            console.log(data);
+            console.log(data.current.temp_f)
+        })
+    }
+    else {
+        alert("Cannot identify city");
+    }
+});
+
